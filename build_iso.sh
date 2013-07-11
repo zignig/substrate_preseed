a3209f9d4d275ddc052b28bcee81291565b27d8c
Simon Kirkby <tigger@interthingy.com>
2013-07-11 09:10:59 -0400

    cleanout

--- build_iso.sh
+++
@@ -1,3 +0,0 @@
-cd ipxe/src
-make bin/ipxe.iso EMBED=../../substrate.ipxe
-mv -f bin/ipxe.iso  ../../substrate.iso
--- proximal.py
+++ proximal.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python
 import web
 #import menu
-import redis,urllib2,string,os,json,uuid,crypt,random,string
+import urllib2,string,os,json,uuid,crypt,random,string
 
-conf = json.load(open('proximal.conf'))
+conf = json.load(open('/opt/substrate_preseed/proximal.conf'))
 print conf
-try:
-	os.stat('cache')
-except:
-	os.mkdir('cache')
-
-r = redis.Redis(conf['redis'],db=2)
+#try:
+#	os.stat('cache')
+#except:
+#	os.mkdir('cache')
 
 urls = (
-	'/debian/pool/(.*)', 'pool',
-	'/debian/dists/(.*)', 'dist',
 	'/postinstall/(.*)','postinstall',
 	'/firstboot/(.*)','firstboot',
 	'/prsd/(.*)','preseed',
@@ -26,14 +22,14 @@ urls = (
 	)
 
 app = web.application(urls, globals())
+
 proxy = conf['mirror']
-render = web.template.render('templates')
+render = web.template.render('/opt/substrate_preseed/templates')
 password = conf['password']
 suite = conf['suite']
 salt_master = conf['salt_master']
 net_boot_path = 'wheezy/main/installer-i386/current/images/netboot/debian-installer/i386/'
 #net_boot_path = 'sid/main/installer-i386/current/images/cdrom/'
-ttl = 86400
 
 def password_hash(password,salt_length=4):
 	salt_string = ''
@@ -44,66 +40,12 @@ def password_hash(password,salt_length=4):
 
 class front_page:
 	def GET(self):
-		#return render.wheezy(password,proxy)
 		return render.frontpage(web.ctx.host)
 
 class machine_type:
 	def GET(self,name):
-		machine_name = 'machine:'+name.split('/')[-1]
 		print(machine_name)
-		r.set(machine_name,name.split('/')[0])
 		return render.ipxe(web.ctx.host,name,'')
-
-class pool:        
-	def GET(self, name): 
-		tmp = string.split(name,'/')
-		if not name:
-			name = 'index.html'
-		if r.exists('pool:'+name):
-			return r.get('pool:'+name)
-		else:
-			cache_file = 'cache'+os.sep+tmp[-1]
-			print cache_file
-			try:
-				print 'open '+cache_file
-				print os.stat(cache_file)
-				f = open(cache_file)
-				data = f.read()
-				r.set('pool:'+name,data)
-				r.expire('pool:'+name,ttl)
-				return data
-			except:
-				print proxy+'/debian/pool/'+name
-				try:
-					req = urllib2.urlopen('http://'+proxy+'/debian/pool/'+name)
-					data = req.read() 
-					print req.info()
-					f = open(cache_file,'w')
-					f.write(data)
-					f.close()
-					r.set('pool:'+name,data)
-					r.expire('pool:'+name,ttl)
-					return data
-				except:
-					return web.notfound()
-				
-class dist:        
-	def GET(self, name): 
-		tmp = string.split(name,'/')
-		if not name:
-			name = 'index.html'
-		if r.exists('dist:'+name):
-			return r.get('dist:'+name)
-		else:
-			try:
-				print proxy+'debian/dists/'+name
-				req = urllib2.urlopen('http://'+proxy+'/debian/dists/'+name)
-				data = req.read()
-				r.set('dist:'+name,data)
-				r.expire('dist:'+name,ttl)
-				return data
-			except:
-				return web.notfound()
 
 class preseed:
 	def GET(self,name):
@@ -118,8 +60,7 @@ class postinstall:
 
 class firstboot:
 	def GET(self,name):
-		machine_name = r.get('machine:'+name.split('/')[-1])			
-		print name,machine_name
+		machine_name = r.get('machine:'+name.split('/')[-1])
 		return render.firstboot(web.ctx.host,salt_master,machine_name)
 
 class chain:
@@ -142,6 +83,7 @@ class boot:
 		r.expire(path,8*ttl)
 		return data
 
+application = web.application(urls, globals()).wsgifunc()
 
 if __name__ == "__main__":
 	app.run()
